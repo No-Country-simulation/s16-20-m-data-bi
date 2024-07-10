9d96651e1f133378b2735982cf10c72e56bbb49c
fevc08 <fevc1988@gmail.com>
2024-07-10 00:01:13 -0400

    Repositorio estructurado

---
+++ structure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Crear directorios principales
+mkdir -p data/{raw,processed,external,webapp}
+mkdir -p notebooks
+mkdir -p src/{data,features,models,visualization}
+mkdir -p tests
+mkdir -p reports/figures
+
+# Crear archivos iniciales
+touch environment.yml
+touch .gitignore
+touch README.md
+
+# Mensaje de finalización
+echo "Estructura del proyecto creada con éxito."
+
+# Opcional: Inicializar un repositorio Git
+git add .
+git commit -m "Repositorio estructurado"
+git push origin github-config
+echo "Repositorio Git actualizado."
